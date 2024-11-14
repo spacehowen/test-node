6ad8aba4423400eeface586f92fedb66d68902f2
howen flores <developer@spacehowen.com>
2024-11-14 07:27:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
-const express = require('express');
 const puppeteer = require('puppeteer');
+const express = require('express');
 
 const app = express();
-let browser;
-
-(async () => {
-    browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-})();
 
 app.get('/generate-iban', async (req, res) => {
-    try {
-        const page = await browser.newPage();
-        await page.goto('http://www.randomiban.com/?country=Italy', { waitUntil: 'networkidle2' });
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const page = await browser.newPage();
+    await page.goto('http://www.randomiban.com/?country=Italy', { waitUntil: 'networkidle2' });
 
-        const iban = await page.evaluate(() => {
-            return document.querySelector('.ibandisplay').innerText;
-        });
+    const iban = await page.evaluate(() => {
+        return document.querySelector('.ibandisplay').innerText;
+    });
 
-        await page.close();
-        res.json({ iban });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Error generating IBAN");
-    }
+    await browser.close();
+    res.json({ iban });
 });
 
 const PORT = process.env.PORT || 3000;
